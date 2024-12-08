d95f167b0529b1d719f71dce348b8a09c769b307
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2024-12-09 00:33:29 +0800

    new

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,20 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public int trap(int[] height) {
-        if(height.length < 3) return 0;
-        int res = 0;
-        Deque<Integer> deque = new LinkedList<>();
-        for (int i = 0; i < height.length; i++) {
-            while (!deque.isEmpty() && height[i] > height[deque.peek()]) {
-                int bottom = deque.pop();
-                int hold = 0;
-                if (!deque.isEmpty()) {
-                    int width = i - deque.peek() - 1;// 宽度
-                    hold = (Math.min(height[deque.peek()], height[i]) - height[bottom]) * width;
-                }
-                res += hold;
-            }
-            deque.push(i);
-        }
+    public int rob(TreeNode root) {
+        if (root == null)  return 0;
+        int[] res = robHelper(root);
+        return Math.max(res[0], res[1]);
+    }
+    private int[] robHelper(TreeNode root){
+        int[] res = new int[2];
+        if(root == null)  return res;
+        int mid = root.val;
+        int[] left_res = robHelper(root.left);
+        int[] right_res = robHelper(root.right);
+        res[0] = Math.max(left_res[0] ,left_res[1]) + Math.max(right_res[0], right_res[1]); // 不偷当前节点
+        res[1] = mid + left_res[0] + right_res[0]; // 偷当前节点
         return res;
     }
 }
