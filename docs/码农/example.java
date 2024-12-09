36c3e653daff1f447ed788d8ee0ac9ddccc691d0
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2024-12-09 20:31:30 +0800

    new

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,32 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    public int rob(TreeNode root) {
-        if (root == null)  return 0;
-        int[] res = robHelper(root);
-        return Math.max(res[0], res[1]);
-    }
-    private int[] robHelper(TreeNode root){
-        int[] res = new int[2];
-        if(root == null)  return res;
-        int mid = root.val;
-        int[] left_res = robHelper(root.left);
-        int[] right_res = robHelper(root.right);
-        res[0] = Math.max(left_res[0] ,left_res[1]) + Math.max(right_res[0], right_res[1]); // 不偷当前节点
-        res[1] = mid + left_res[0] + right_res[0]; // 偷当前节点
-        return res;
+    public int maxProfit(int[] prices, int fee) {
+        if(prices == null || prices.length < 2) return 0;
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = -prices[0] - fee;
+        dp[0][1] = 0;
+        for(int i = 1; i < prices.length; i++){
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i] - fee);
+            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
+        }
+        return dp[prices.length-1][1];
     }
 }
