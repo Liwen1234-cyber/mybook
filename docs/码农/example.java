a0c382002947676fb6adef798bccad9cd8f8648b
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2024-12-10 00:49:12 +0800

    new

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,13 +1,6 @@
 class Solution {
-    public int maxProfit(int[] prices, int fee) {
-        if(prices == null || prices.length < 2) return 0;
-        int[][] dp = new int[prices.length][2];
-        dp[0][0] = -prices[0] - fee;
-        dp[0][1] = 0;
-        for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i] - fee);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
-        }
-        return dp[prices.length-1][1];
+    public int findLength(int[] nums1, int[] nums2) {
+        if(nums1.length == 0 || nums2.length == 0) return 0;
+        
     }
 }
