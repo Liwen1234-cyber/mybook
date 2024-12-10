7f21e3f0c08948f4c770cc5d29cd7280fe82e55b
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2024-12-10 18:32:23 +0800

    new

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,6 +1,16 @@
-class Solution {
-    public int findLength(int[] nums1, int[] nums2) {
-        if(nums1.length == 0 || nums2.length == 0) return 0;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        List<List<Integer>> list = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt(); // 节点
+        int M = in.nextInt(); // 边
+        int[][] edges = new int[M][2];
+        for(int i = 0; i < M; i++){
+            edges[i][0] = in.nextInt();
+            edges[i][1] = in.nextInt();
+        }
 
     }
 }
