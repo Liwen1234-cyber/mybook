9109cd013c5bac64865b688fcc98036fedf91cae
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-01-18 03:11:30 +0800

    new

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ module.exports = {
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
         hashed: true,
-        language: ['zh'],
+        language: ['en', 'zh'],
         highlightSearchTermsOnTargetPage: true,
         explicitSearchResultPath: true,
         docsRouteBasePath: '/',
