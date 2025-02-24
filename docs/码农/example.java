5caa204c78b07c1f705733028f5c5f38fe22f735
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-02-24 14:32:55 +0800

    New

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,58 +1,56 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main{
     public static int[][] dir = {{1,0},{-1,0},{0,1},{0,-1}};
-    public static int count = 0;
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        int[][] arr = new int[N][M];
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                arr[i][j] = in.nextInt();
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+        int[][] grid = new int[N][M];
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                grid[i][j] = scanner.nextInt();
             }
-        }
+        } 
 
-        in.close();
-
-        // 从左边界和右边界向中间遍历
-        for(int i = 0; i < N; i++){
-            if(arr[i][0] == 1) dfs(i, 0, arr);
-            if(arr[i][M-1] == 1) dfs(i, M-1, arr);
-        }
-
-        // 从上边界和下边界向中间遍历
-        for(int j = 0; j < M; j++){ // 遍历上侧和下侧边界
-            if(arr[0][j] == 1) dfs(0, j, arr);
-            if(arr[N-1][j] == 1) dfs(N-1, j, arr);
-        }
-
-        count = 0;
         
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(arr[i][j] == 1) dfs(i, j, arr);
+        for (int i = 0; i < N; i++){
+            if(grid[i][0] == 1) dfs(grid, i, 0);
+            if(grid[i][M-1] == 1) dfs(grid, i, M-1);
+        }
+        
+        for (int j = 0; j < M; j++){
+            if(grid[0][j] == 1) dfs(grid, 0, j);
+            if(grid[N-1][j] == 1) dfs(grid, N-1, j);
+        }
+
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                if(grid[i][j] == 1) grid[i][j] = 0;
+                else if(grid[i][j] == 2) grid[i][j] = 1;
             }
         }
 
-        System.out.println(count);
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+
     }
     
+    private static void dfs(int[][] grid, int x, int y){
 
-    public static void dfs(int x, int y, int[][] arr){
-        arr[x][y] = 0;
-        count++;
-
+        grid[x][y] = 2;
+        
         for(int i = 0; i < 4; i++){
-            int newX = x + dir[i][0];
-            int newY = y + dir[i][1]; 
-
-            if(newX < 0 || newX >= arr.length || newY < 0 || newY >= arr[0].length)
-                continue;
-
-            if(arr[newX][newY] == 1){
-                dfs(newX, newY, arr);
+            int next_x = x + dir[i][0];
+            int next_y = y + dir[i][1];
+            if(next_x >= 0 && next_x < grid.length && next_y >= 0 && next_y < grid[0].length && grid[next_x][next_y] == 1){
+                dfs(grid, next_x, next_y);
             }
         }
     }
