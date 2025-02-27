6b3b361a2e32aa2ad11b50c8be14a6229cca854a
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-02-27 17:57:51 +0800

    New

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -3,53 +3,46 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        List<List<Integer>> dir = new ArrayList<>();
         int points = scanner.nextInt();
         int edges = scanner.nextInt();
-        for(int i = 0; i < points; i++){
-            dir.add(new ArrayList<>());
-        }
+        Disjoint disjoint = new Disjoint(points+1);
         for(int i = 0; i < edges; i++){
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-            dir.get(a-1).add(b-1);
+            disjoint.join(scanner.nextInt(), scanner.nextInt());
+        
         }
-        boolean[] visited = new boolean[points];
-        // dfs(dir, visited, 0);
-        bfs(dir, visited, 0);
-        for(int i = 0; i < visited.length; i++){
-            if(!visited[i]){
-                System.out.println(-1);
-                return;
-            }
+
+        if(disjoint.isSame(scanner.nextInt(), scanner.nextInt())){
+            System.out.println(1);
         }
-        System.out.println(1);
+        else{
+            System.out.println(0);
+        }
 
         scanner.close();
     }
+}
 
-    private static void dfs(List<List<Integer>> dir, boolean[] visited, int start){
-        if(visited[start]) return;
-        visited[start] = true;
-        for(int i : dir.get(start)){
-            dfs(dir, visited, i);
-        }
+class Disjoint{
+    private int[] father;
 
-    }
-
-    private static void bfs(List<List<Integer>> dir, boolean[] visited, int start){
-        Deque<Integer> dequeue = new LinkedList<>();
-        dequeue.offer(start);
-        visited[start] = true;
-        while(!dequeue.isEmpty()){
-            int cur = dequeue.pollLast();
-            for(int i : dir.get(cur)){
-                if(!visited[i]){
-                    dequeue.offer(i);
-                    visited[i] = true;
-                }
-            }
+    public Disjoint(int n){
+        father = new int[n];
+        for(int i = 0; i < n; i++){
+            father[i] = i;
         }
     }
-    
+
+    public int find(int x){
+        return father[x] == x ? x : find(father[x]);
+    }
+
+    public void join(int x, int y){ // x -> y
+        int fx = find(x);
+        int fy = find(y);
+        father[fx] = fy;
+    }
+
+    public boolean isSame(int x, int y){
+        return find(x) == find(y);
+    }
 }
