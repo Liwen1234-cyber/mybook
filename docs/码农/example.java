babbe1db064f911654d9480cf2a37b7d771c16ed
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-03-11 14:01:27 +0800

    New

--- docs/码农/example.java
+++ docs/码农/example.java
@@ -1,14 +1,52 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0 || nums == null) return new int[]{-1, -1};
+    public int max, len;
+    public Set<String> set = new HashSet<>();
+    public String ss;
 
-        int left = 0, right = nums.length - 1;
-        while(left <= right && nums[left] != nums[right]){
-            if(nums[left] < target) left++;
-            if(nums[right] > target) right--;
+    public List<String> removeInvalidParentheses(String s) {
+        ss = s;
+
+        int l = 0, r = 0; // l: 多余左括号的数量，r: 多余右括号的数量
+        for(char i : s.toCharArray()){
+            if(i == '(') l++;
+            else if(i == ')'){
+                if(l > 0) l--;
+                else r++;
+            }
+        }
+        len = s.length() - l - r;
+
+        int cur_l = 0, cur_r = 0;
+        for(char i : s.toCharArray()){
+            if(i == '(') cur_l++;
+            else if(i == ')') cur_r++;
+        }
+        max = Math.min(cur_l - l, cur_r - r);
+
+        dfs("", 0, 0, l, r);
+        return new ArrayList<>(set);
+
+    }
+
+    public void dfs(String cur, int index, int score, int l, int r){ 
+        if(score < 0 || score > max || l < 0 || r < 0) return;
+
+        if(index == ss.length()){
+            if(score == 0 && cur.length() == len) set.add(cur);
+            return;
         }
 
-        if(nums[left] == target && nums[right] == target) return new int[]{left, right};
-        else return new int[]{-1, -1};
+        if(ss.charAt(index) == '('){
+            dfs(cur + "(", index + 1, score + 1, l, r);
+            dfs(cur, index + 1, score, l - 1, r);
+        }
+        else if(ss.charAt(index) == ')'){
+            dfs(cur + ")", index + 1, score - 1, l, r);
+            dfs(cur, index + 1, score, l, r - 1);
+        }
+        else{
+            dfs(cur + ss.charAt(index), index + 1, score, l, r);
+        }
+
     }
 }
