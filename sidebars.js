3bce272e707bb738d475c9903b4a2e85b9cba0a0
LiWen <175311843+Liwen1234-cyber@users.noreply.github.com>
2025-05-01 20:55:26 +0800

    New

--- sidebars.js
+++ sidebars.js
@@ -140,7 +140,7 @@ module.exports = {
           'Learning.',
       },
       items: [
-        '学习/周报/index',
+        '周报/index',
         '学习/克拉美-罗界',
         '学习/Csi-feedback',
         '学习/原型验证软件平台_总结',
